662cd13b71e6741e1423f461d7e792d9be2b6a80
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-16 15:08:24 -0300

    labfood1

---
+++ 4food/src/Pages/TelaInicial/TelaInicial.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TelaInicialContainer } from './Styled';
+
+
+
+export const TelaInicial = () => {
+
+
+
+    return (
+
+
+        <TelaInicialContainer>
+      <h1> Bem vindos ao 4food</h1>
+        </TelaInicialContainer>
+
+     );  
+}
+
+
+export default TelaInicial 
---
+++ 4food/src/Pages/TelaInicial/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const TelaInicialContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    ;
+    `
+
