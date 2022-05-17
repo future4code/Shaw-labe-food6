383004348395fcded05898ffd160f1a6109e486c
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-17 11:01:44 -0300

    labfood2

--- 4food/src/App.js
+++ 4food/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Components/Header/Header';
+import BarraBusca from './Components/BarraRestaurante/BarraBuscaRestaurante'
+import CardBusca from './Components/cardBusca/CardBusca';
 
 
 function App() {
   return (
    <div>
-     olá mundo
+     <Header/>
+     <BarraBusca/>
+     <CardBusca/>
    </div>
   );
 }
